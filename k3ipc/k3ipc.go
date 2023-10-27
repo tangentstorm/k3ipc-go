c8e55752ba66ec8243c31a8b710769d150feca6e
tangentstorm <michal.wallace@gmail.com>
2023-10-27 20:26:39 +0000

    add all tests. implement CHR,CHRs,SYM,NUL

--- k3ipc/k3ipc.go
+++ k3ipc/k3ipc.go
@@ -1,10 +1,10 @@
 package k3ipc
 
 import (
+	"C"
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"strconv"
 	"strings"
 )
@@ -12,13 +12,19 @@ import (
 const (
 	K3LST = iota
 	K3INT
-	K3NUM
+	K3FLT
 	K3CHR
 	K3SYM
 	K3DCT
 	K3NUL
 	K3FUN
 )
+
+// Arbitrary wrapper to mark incoming string as a symbol
+type KSym struct{ s string }
+
+func sym(s string) KSym       { return KSym{s} }
+func (s KSym) String() string { return "`" + s.s }
 
 // numstrToBytes
 // convert a string of space-separated numbers into a byte array
@@ -42,61 +48,103 @@ func BytesToNumStr(bytes []byte) string {
 	return strings.Join(nums, " ")
 }
 
+type MsgHeader struct {
+	byteOrder binary.ByteOrder
+	msgType   byte
+	msgLen    int32
+	dataType  int32
+}
+
+func parseMessageHeader(r *bytes.Reader) (h MsgHeader) {
+	endianFlag, _ := r.ReadByte()
+	h.byteOrder = binary.LittleEndian
+	if endianFlag != 1 {
+		h.byteOrder = binary.BigEndian
+	}
+	r.ReadByte() // ignore
+	r.ReadByte() // ignore
+	h.msgType, _ = r.ReadByte()
+	binary.Read(r, h.byteOrder, &h.msgLen)
+	binary.Read(r, h.byteOrder, &h.dataType)
+	return
+}
+
 // K3 data from bytes
 func Db(buf []byte) any {
-	// k3 buffer format (used by serialization and IPC):
-	var endianFlag byte
-	// then 2 ignored bytes, then 1 byte for k-ipc message type (which we don't need here)
-	var len int32
-	var typ int32
-
 	r := bytes.NewReader(buf)
-
-	endianFlag, _ = r.ReadByte()
-	if endianFlag != 1 {
-		panic("endianFlag != 1")
-	}
-
-	// fast forward past the bytes we don't care about
-	_, err := r.Seek(4, io.SeekStart)
-	if err != nil {
-		fmt.Println("Seek failed")
-		return nil
-	}
-
-	eness := binary.LittleEndian
-	binary.Read(r, eness, &len)
-	binary.Read(r, eness, &typ)
-
-	switch typ {
+	h := parseMessageHeader(r)
+	const dataStart = 16
+	switch h.dataType {
 	case K3INT:
 		var res int32
-		binary.Read(r, eness, &res)
+		binary.Read(r, h.byteOrder, &res)
 		return res
+	case K3FLT:
+		var res C.double
+		binary.Read(r, h.byteOrder, &res)
+		return float64(res)
+	case K3CHR:
+		var res byte
+		res, _ = r.ReadByte()
+		return res
+	case -K3CHR:
+		var res string
+		len := bytes.IndexByte(buf[dataStart:], 0)
+		res = string(buf[dataStart : dataStart+len])
+		return res
+	case K3SYM:
+		// symbol doesn't include length, so data starts at 12
+		// and the msgLen is 4 bytes longer, not 8
+		// then there's a null terminator so subtract 1 for that too
+		return sym(string(buf[12 : 12+h.msgLen-5]))
+	case K3NUL:
+		return nil
 	default:
-		panic("TODO: parse type #" + fmt.Sprintf("%v", typ))
+		panic("TODO: parse type #" + fmt.Sprintf("%v", h.dataType))
 	}
 }
 
 // K3 data to bytes
-func Bd(val any) (buf []byte) {
+func Bd(val any) (res []byte) {
+	ord := binary.LittleEndian
+	buf := bytes.NewBuffer([]byte{
+		1, 0, 0, 0, // message type
+		0, 0, 0, 0, // message length
+		0, 0, 0, 0, // data type
+	})
+	const tLen = 4
+	dLen, dTyp := 0, K3INT
+	const dStart = 12
 	switch val := val.(type) {
 	case int32:
-		buf = make([]byte, 16)
-		buf[0] = 1
-		len := 8 // length of the data (4 byte type + 4 byte value)
-		endy := binary.LittleEndian
-		endy.PutUint32(buf[4:], uint32(len))
-		endy.PutUint32(buf[8:], uint32(K3INT))
-		endy.PutUint32(buf[12:], uint32(val))
-		return buf
+		dLen = 4
+		buf.Write([]byte{0, 0, 0, 0})
+		ord.PutUint32(buf.Bytes()[dStart:], uint32(val))
 	case byte:
-		return []byte{
-			1, 0, 0, 0,
-			8, 0, 0, 0,
-			byte(K3CHR), 0, 0, 0,
-			val, 0, 0, 0}
+		dTyp, dLen = K3CHR, 4
+		buf.Write([]byte{val, 0, 0, 0})
+	case KSym:
+		dTyp, dLen = K3SYM, len(val.s)+1
+		// no string length for symbols
+		buf.Write([]byte(val.s))
+		buf.WriteByte(0)
+	case string:
+		dTyp = -K3CHR
+		dLen = 4 + len(val) + 1 // strlen + \0
+		// store the string length:
+		buf.Write([]byte{0, 0, 0, 0})
+		ord.PutUint32(buf.Bytes()[dStart:], uint32(dLen-5))
+		buf.Write([]byte(val))
+		buf.WriteByte(0)
 	default:
-		panic("don't know how to convert value to binary!")
+		if val == nil {
+			dTyp, dLen = K3NUL, 4
+			buf.Write([]byte{0, 0, 0, 0})
+			break
+		}
+		panic(val)
 	}
+	ord.PutUint32(buf.Bytes()[4:], uint32(dLen+tLen))
+	ord.PutUint32(buf.Bytes()[8:], uint32(dTyp))
+	return buf.Bytes()
 }
--- k3ipc/k3ipc_test.go
+++ k3ipc/k3ipc_test.go
@@ -1,20 +1,8 @@
 package k3ipc
 
 import (
-	"fmt"
 	"testing"
 )
-
-type Sym struct{ s string }
-
-func sym(s string) Sym       { return Sym{s} }
-func (s Sym) String() string { return "`" + s.s }
-
-func ExampleK3List() {
-	ls := []any{"cat", 123, sym("cat"), []int{1, 2, 3}}
-	fmt.Printf("%v\n", ls)
-	// Output: [cat 123 `cat [1 2 3]]
-}
 
 func check(t *testing.T, expectValue any, expectedBytes string) {
 	actualValue := Db(NumStrToBytes(expectedBytes))
@@ -23,13 +11,47 @@ func check(t *testing.T, expectValue any, expectedBytes string) {
 	}
 	actualBytes := BytesToNumStr(Bd(expectValue))
 	if actualBytes != expectedBytes {
-		t.Errorf("Bd() failed. Expected %v but got %v", expectedBytes, actualBytes)
+		t.Errorf("Bd() failed.\nexpect: %v\nactual: %v", expectedBytes, actualBytes)
 	}
 }
-
-func TestK3(t *testing.T) {
+func TestK3INT(t *testing.T) {
 	check(t, int32(0), "1 0 0 0 8 0 0 0 1 0 0 0 0 0 0 0")
 	check(t, int32(1), "1 0 0 0 8 0 0 0 1 0 0 0 1 0 0 0")
 	check(t, int32(0x7ffffffe), "1 0 0 0 8 0 0 0 1 0 0 0 254 255 255 127")
 	check(t, int32(1234), "1 0 0 0 8 0 0 0 1 0 0 0 210 4 0 0")
+}
+func TestK3FLT(t *testing.T) {
+	check(t, float64(1.1), "1 0 0 0 16 0 0 0 2 0 0 0 1 0 0 0 154 153 153 153 153 153 241 63")
+}
+func TestK3CHR(t *testing.T) {
+	check(t, byte('x'), "1 0 0 0 8 0 0 0 3 0 0 0 120 0 0 0")
+}
+func TestK3CHRs(t *testing.T) {
+	check(t, "hello",
+		"1 0 0 0 14 0 0 0 253 255 255 255 5 0 0 0 104 101 108 108 111 0")
+	check(t, "hi",
+		"1 0 0 0 11 0 0 0 253 255 255 255 2 0 0 0 104 105 0")
+}
+func TestK3SYM(t *testing.T) {
+	check(t, sym("abc"), "1 0 0 0 8 0 0 0 4 0 0 0 97 98 99 0")
+}
+func TestK3SYMs(t *testing.T) {
+	check(t, []KSym{sym("abc"), sym("xyz")},
+		"1 0 0 0 16 0 0 0 252 255 255 255 2 0 0 0 97 98 99 0 120 121 122 0")
+}
+func TestK3NUL(t *testing.T) {
+	check(t, nil, "1 0 0 0 8 0 0 0 6 0 0 0 0 0 0 0")
+}
+func TestK3LST(t *testing.T) {
+	check(t, []any{}, "1 0 0 0 8 0 0 0 0 0 0 0 0 0 0 0")
+	check(t, []any{"abc", 1},
+		"1 0 0 0 32 0 0 0 0 0 0 0 2 0 0 0 253 255 255 255 3 0 0 0 97 98 99 0 0 0 0 0 1 0 0 0 1 0 0 0")
+	check(t, []any{'a', 'b', 'c', 1},
+		"1 0 0 0 40 0 0 0 0 0 0 0 4 0 0 0 3 0 0 0 97 0 0 0 3 0 0 0 98 0 0 0 3 0 0 0 99 0 0 0 1 0 0 0 1 0 0 0")
+	check(t, []any{[]any{[]any{}, []any{}}},
+		"1 0 0 0 32 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
+}
+func TestK3DCT(t *testing.T) {
+	check(t, map[string]any{}, "1 0 0 0  8 0 0 0 5 0 0 0 0 0 0 0")
+	check(t, map[string]any{"k": 123}, "1 0 0 0 40 0 0 0 5 0 0 0 1 0 0 0 0 0 0 0 3 0 0 0 4 0 0 0 107 0 0 0 1 0 0 0 123 0 0 0 6 0 0 0 0 0 0 0")
 }
